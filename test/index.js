eb8ecf9f0bc6b656a896cf7127be1701f79f0fc7
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2016-07-09 20:11:27 +0200

    test(): update tests

--- test/index.js
+++ test/index.js
@@ -10,15 +10,16 @@ const { join } = require('path')
 const { readFileSync } = require('fs')
 
 const fixtures = (file) => readFileSync(join(__dirname, 'fixtures', file), 'utf8')
-const expected = (file) => readFileSync(join(__dirname, 'expected', file), 'utf8')
+const expected = (file) => readFileSync(join(__dirname, 'expects', file), 'utf8')
 
 const posthtml = require('posthtml')
 const plugin = require('..')
 
-test('Imports should result as expected', (t) => {
-  posthtml([ plugin({root: './fixtures/imports'}) ])
-    .process(fixtures('./fixtures/index.html'))
+test('imports should work as expected', (t) => {
+  posthtml([ plugin({root: 'fixtures/imports'}) ])
+    .process(fixtures('index.html'))
     .then((result) => {
-      t.is(expected('./expect/index.html'), result.html)
+      console.log(result.html)
+      t.is(expected('index.html'), result.html)
     })
 })
